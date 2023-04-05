d45776ec0a8f8e56e10084801877af68d0de6ddb
Jordan Taylor <jtaylor81284@hotmail.com>
2023-04-05 17:41:40 +0100

    Add function to get best orf for each transcript
    Simple function that translates each transcript and returns the
    longest ORF. This is added as a column to the final transcript data.

--- combine_transcript_data.py
+++ combine_transcript_data.py
@@ -1,6 +1,26 @@
 import csv
 import argparse
+import re
 from Bio import SeqIO
+from Bio.Seq import Seq
+
+def find_longest_orf(sequence):
+    longest_orf = ''
+    for frame in range(3):
+        translated = Seq(sequence[frame:]).translate(cds=False)
+        orfs = re.findall(r'(?=(M[^*]*))', str(translated))
+        for orf in orfs:
+            if len(orf) > len(longest_orf):
+                longest_orf = orf
+
+    for frame in range(3):  # Iterate through three reading frames for the reverse strand
+        rev_translated = Seq(sequence[:len(sequence)-frame]).reverse_complement().translate(cds=False)
+        rev_orfs = re.findall(r'(?=(M[^*]*))', str(rev_translated))
+        for orf in rev_orfs:
+            if len(orf) > len(longest_orf):
+                longest_orf = orf
+
+    return longest_orf
 
 def combine_data(fasta_file, quant_file, output_file):
     # Read the fasta file
@@ -12,12 +32,14 @@ def combine_data(fasta_file, quant_file, output_file):
     with open(quant_file) as quant, open(output_file, 'w') as output:
         csv_reader = csv.DictReader(quant, delimiter='\t')
         csv_writer = csv.writer(output)
-        csv_writer.writerow(['transcript_id', 'sequence', 'length', 'effectivelength', 'tpm', 'numreads'])
+        csv_writer.writerow(['transcript_id', 'sequence', 'length', 'effectivelength', 'tpm', 'numreads', 'longest_coding_orf'])
 
         for row in csv_reader:
             transcript_id = row['Name']
             if transcript_id in sequences:
-                csv_writer.writerow([transcript_id, sequences[transcript_id], row['Length'], row['EffectiveLength'], row['TPM'], row['NumReads']])
+                sequence = sequences[transcript_id]
+                longest_orf = find_longest_orf(sequence)
+                csv_writer.writerow([transcript_id, sequence, row['Length'], row['EffectiveLength'], row['TPM'], row['NumReads'], longest_orf])
             
 def main():
     parser = argparse.ArgumentParser(description='Combine fasta sequences and quant.sf data into a CSV file.')
